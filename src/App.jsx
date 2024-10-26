e7aa90c68ac2c27e37a2da87627c693ed2ad2ffd
wagnersrj <wagner.ramos2025@gmail.com>
2024-10-26 12:00:44 -0300

    adição de novos arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,12 @@ box-sizing: border-box
 body{
 background-image: url("https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/Imagem%20palacio.png?raw=true");
 background-repeat: no-repeat;
-background-size: 100% 100%;
+background-size: cover;
 background-position: center;
 height: 100vh;
+
+@media (min-width: 320px) and (max-width: 399px) {
+    
 
 }
 
@@ -30,7 +33,7 @@ display: flex;
 align-items: center;
 justify-content: center;
 flex-direction: column;
-margin-top: 40px;
+margin-top: 2rem;
 
 `
 
@@ -40,6 +43,30 @@ font-family: "Sevillana", cursive;
 color: white;
 font-weight: 400;
 font-size: 62px;
+
+@media (min-width: 300px) and (max-width: 360px) {
+font-size: 16px;
+text-align:center;
+
+}
+
+@media (min-width: 361px) and (max-width: 500px) {
+font-size: 25px;
+text-align:center;
+
+}
+
+@media (min-width: 500px) and (max-width: 1000px) {
+font-size: 45px;
+text-align:center;
+
+}
+
+@media (min-width: 1000px)  {
+
+text-align:center;
+
+
 `
 
 const Button = styled.button `
@@ -53,16 +80,35 @@ padding: 0.5em 3em;
 background-color:  #007BFF;
 border: none;
 
+@media (min-width: 300px) and (max-width: 600px){
+font-size: 20px;
+font-weight: 300;
+border-radius: 40px;
+padding: 20px 60px;
+background-color:  #B74794;
+
+
+}
+
+
 
 
 
 `
 const Img = styled.img `
 
-width: 772px;
-height: 728px;
+
+
+@media (min-width: 300px) and (max-width: 399px){
+
+
+width: 600px;
+
+
+}
 
 `
+
 
 
 
